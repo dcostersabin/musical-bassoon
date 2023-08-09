8327609a76b72eeb07bf4be2932a05475ff79b8e
Sabin Sharma <dcoster.sabin@gmail.com>
2023-08-09 12:47:04 +0545

    Added User Update Scheme

--- models/scheme.py
+++ models/scheme.py
@@ -32,3 +32,18 @@ class MusicScheme(Schema):
     genre = fields.Str(
         required=True, validate=OneOf(["country", "classic", "rock", "jazz"])
     )
+
+
+class UserUpdateScheme(Schema):
+    first_name = fields.Str(required=True, validate=Length(min=3, max=255))
+    last_name = fields.Str(required=True, validate=Length(min=3, max=255))
+    phone = fields.Str(
+        required=True,
+        validate=[
+            Length(min=10, max=14),
+            Regexp(regex="^([0|\+[0-9]{1,5})?([7-9][0-9]{9})$"),
+        ],
+    )
+    dob = fields.Date(required=True)
+    gender = fields.Str(required=True, validate=OneOf(["M", "F"]))
+    address = fields.Str(required=True, validate=Length(min=3, max=255))
